a227d2835cf916bc06edf97e1594999f229809dc
Rohit Kamle <rktf2020@gmail.com>
2024-01-01 11:16:17 +0400

    Final commit

--- gloud-commands.sh
+++ gloud-commands.sh
@@ -21,8 +21,6 @@ gcloud iam service-accounts create $SA_NAME --description="Service account for T
 
 # ASSIGN PERSMISSIONS TO SERVICE ACCOUNT
 gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:$SA_NAME@$PROJECT_ID.iam.gserviceaccount.com" --role="roles/container.admin"
-gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:$SA_NAME@$PROJECT_ID.iam.gserviceaccount.com" --role="roles/iam.serviceAccountTokenCreator"
-gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:$SA_NAME@$PROJECT_ID.iam.gserviceaccount.com" --role="roles/iam.serviceAccountUser"
 
 # CREATED MANAGED IDENTITY POOL AND PROVIDER
 gcloud iam workload-identity-pools create $WIF_POOL_NAME --project $PROJECT_ID --location "global" --display-name "$WIF_POOL_DISPLAY_NAME"
